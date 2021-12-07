1a6ef0a8a94079a52da50b45abf1b53c2ce25525
boyoungna <kby52082933@gmail.com>
2021-12-07 16:00:36 +0900

    4

---
+++ js/1207_14.js
@@ -0,0 +1,11 @@
+'use sctict';
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        dovument.getElementById("navbar").style.top = "-50px";
+    }  
+}
