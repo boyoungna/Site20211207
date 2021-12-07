eab3d569f7544d020633a00b37a8772c8c03dfed
boyoungna <kby52082933@gmail.com>
2021-12-07 14:39:27 +0900

    333

---
+++ js/1207_9.js
@@ -0,0 +1,9 @@
+'use strict';
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft= "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
