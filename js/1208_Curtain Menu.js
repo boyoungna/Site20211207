1361b1ca409ab757f227375a777acec5c0e72b96
boyoungna <kby52082933@gmail.com>
2021-12-08 12:15:47 +0900

    22

---
+++ js/1208_Curtain Menu.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
