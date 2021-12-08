7861958846cf441264bad5440f2951fbb7f0b542
boyoungna <kby52082933@gmail.com>
2021-12-08 10:26:45 +0900

    1

--- js/1207_Shrink Navbar on Scroll.js
+++ js/1207_Shrink Navbar on Scroll.js
@@ -2,12 +2,12 @@
 
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction(){
+function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.documentElement.scrollTop > 80 {
             document.getElementById("navbar").style.padding = "30px 10px";
             document.getElementById("logo").style.fontSize = "25px";
-        } else {
+            } else {
             document.getElementById("navbar").style.padding = "80px 10px";
             dovument.getElementById("logo").style.fontSize = "35px"
         }
